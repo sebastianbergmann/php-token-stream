57a40b185f1390ea5dac707fda92661acc9306ec
Sebastian Bergmann <sb@sebastian-bergmann.de>
2010-01-25 22:43:45 +0100

    Initial work on CLI tool.

---
+++ PHP/Token/Stream/TextUI/Command.php
@@ -0,0 +1,183 @@
+<?php
+/**
+ * php-token-stream
+ *
+ * Copyright (c) 2009-2010, Sebastian Bergmann <sb@sebastian-bergmann.de>.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *
+ *   * Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimer in
+ *     the documentation and/or other materials provided with the
+ *     distribution.
+ *
+ *   * Neither the name of Sebastian Bergmann nor the names of his
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ * @package   PHP_TokenStream
+ * @author    Sebastian Bergmann <sb@sebastian-bergmann.de>
+ * @copyright 2009-2010 Sebastian Bergmann <sb@sebastian-bergmann.de>
+ * @license   http://www.opensource.org/licenses/bsd-license.php  BSD License
+ * @since     File available since Release 1.0.0
+ */
+
+require_once 'PHP/Token/Stream.php';
+require_once 'ezc/Base/base.php';
+
+function __autoload($className)
+{
+    ezcBase::autoload($className);
+}
+
+/**
+ * TextUI frontend for PHP_TokenStream.
+ *
+ * @author    Sebastian Bergmann <sb@sebastian-bergmann.de>
+ * @copyright 2009-2010 Sebastian Bergmann <sb@sebastian-bergmann.de>
+ * @license   http://www.opensource.org/licenses/bsd-license.php  BSD License
+ * @version   Release: @package_version@
+ * @link      http://github.com/sebastianbergmann/php-token-stream/tree
+ * @since     Class available since Release 1.0.0
+ */
+class PHP_Token_Stream_TextUI_Command
+{
+    /**
+     * Main method.
+     */
+    public static function main()
+    {
+        $input  = new ezcConsoleInput;
+        $output = new ezcConsoleOutput;
+
+        $input->registerOption(
+          new ezcConsoleOption(
+            'h',
+            'help',
+            ezcConsoleInput::TYPE_NONE,
+            NULL,
+            FALSE,
+            '',
+            '',
+            array(),
+            array(),
+            FALSE,
+            FALSE,
+            TRUE
+           )
+        );
+
+        $input->registerOption(
+          new ezcConsoleOption(
+            'v',
+            'version',
+            ezcConsoleInput::TYPE_NONE,
+            NULL,
+            FALSE,
+            '',
+            '',
+            array(),
+            array(),
+            FALSE,
+            FALSE,
+            TRUE
+           )
+        );
+
+        try {
+            $input->process();
+        }
+
+        catch (ezcConsoleOptionException $e) {
+            print $e->getMessage() . "\n";
+            exit(1);
+        }
+
+        if ($input->getOption('help')->value) {
+            self::showHelp();
+            exit(0);
+        }
+
+        else if ($input->getOption('version')->value) {
+            self::printVersionString();
+            exit(0);
+        }
+
+        $arguments = $input->getArguments();
+
+        if (empty($arguments)) {
+            self::showHelp();
+            exit(1);
+        }
+
+        self::printVersionString();
+
+        print "Line   Token                          Text\n" .
+              str_repeat('-', 79) . "\n";
+
+        foreach (new PHP_Token_Stream($arguments[0]) as $token) {
+            if ($token instanceof PHP_Token_WHITESPACE) {
+                $text = '';
+            } else {
+                $text = str_replace(array("\r", "\n"), '', (string)$token);
+
+                if (strlen($text) > 40) {
+                    $text = explode("\n", wordwrap($text, 40));
+                    $text = $text[0];
+                }
+            }
+
+            printf(
+              "%5d  %-30s %s\n",
+              $token->getLine(),
+              str_replace('PHP_Token_', '', get_class($token)),
+              $text
+            );
+        }
+    }
+
+    /**
+     * Shows the help.
+     */
+    protected static function showHelp()
+    {
+        self::printVersionString();
+
+        print <<<EOT
+Usage: phptok [switches] <file>
+
+  --help                   Prints this usage information.
+  --version                Prints the version and exits.
+
+EOT
+;
+    }
+
+    /**
+     * Prints the version string.
+     */
+    protected static function printVersionString()
+    {
+        print "phptok @package_version@ by Sebastian Bergmann.\n\n";
+    }
+}
+?>
