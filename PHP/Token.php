0178e3d1600826b4e0559532da49dac7504e15c7
Laurent Laville <pear@laurent-laville.org>
2011-01-07 12:33:06 +0100

    FIX issue8 regression with PHP_CodeCoverage Report CloverTest

--- PHP/Token.php
+++ PHP/Token.php
@@ -199,7 +199,9 @@ abstract class PHP_TokenWithScope extends PHP_Token
 
             else if ($this instanceof PHP_Token_FUNCTION &&
                 $tokens[$i] instanceof PHP_Token_SEMICOLON) {
-                $this->endTokenId = $i;
+                if ($block === 0) {
+                    $this->endTokenId = $i;
+                }
             }
 
             $i++;
