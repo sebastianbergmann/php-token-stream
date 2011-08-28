78991670e1e133455e2969e74bf03fd5477d4ca5
Sebastian Bergmann <sb@sebastian-bergmann.de>
2011-08-28 19:41:31 +0200

    Fix CS/WS issues.

--- PHP/Token.php
+++ PHP/Token.php
@@ -121,26 +121,29 @@ abstract class PHP_TokenWithScope extends PHP_Token
      */
     public function getDocblock()
     {
-        $tokens = $this->tokenStream->tokens();
+        $tokens            = $this->tokenStream->tokens();
         $currentLineNumber = $tokens[$this->id]->getLine();
-        $prevLineNumber = $currentLineNumber - 1;
+        $prevLineNumber    = $currentLineNumber - 1;
 
         for ($i = $this->id - 1; $i; $i--) {
             if (isset($tokens[$i])) {
                 if ($tokens[$i] instanceof PHP_Token_FUNCTION ||
                     $tokens[$i] instanceof PHP_Token_CLASS) {
-                    // Some other class or function, no docblock can be used for the current token
+                    // Some other trait, class or function, no docblock can be
+                    // used for the current token
                     break;
                 }
 
                 $line = $tokens[$i]->getLine();
 
                 if ($line == $currentLineNumber ||
-                    ($line == $prevLineNumber && ($tokens[$i] instanceof PHP_Token_WHITESPACE))) {
+                    ($line == $prevLineNumber &&
+                     $tokens[$i] instanceof PHP_Token_WHITESPACE)) {
                     continue;
                 }
 
-                if ($line < $currentLineNumber && !($tokens[$i] instanceof PHP_Token_DOC_COMMENT)) {
+                if ($line < $currentLineNumber &&
+                    !$tokens[$i] instanceof PHP_Token_DOC_COMMENT) {
                     break;
                 }
 
