9de1b063746b6c114f736ca18ea9469f595541cd
Sebastian Bergmann <sb@sebastian-bergmann.de>
2015-06-13 08:33:19 +0200

    Remove obsolete annotation

--- src/Token/Stream.php
+++ src/Token/Stream.php
@@ -14,7 +14,6 @@
  * @author    Sebastian Bergmann <sebastian@phpunit.de>
  * @copyright Sebastian Bergmann <sebastian@phpunit.de>
  * @license   http://www.opensource.org/licenses/BSD-3-Clause  The BSD 3-Clause License
- * @version   Release: @package_version@
  * @link      http://github.com/sebastianbergmann/php-token-stream/tree
  * @since     Class available since Release 1.0.0
  */
