sebastianbergmann/php-token-stream

commit 6f1447a60f068c89d345ceee32025a51dd630f1c

Closes #52

src/Token/Stream.php
@@ -352,8 +352,8 @@ class PHP_Token_Stream implements ArrayAccess, Countable, SeekableIterator
         $this->classes    = array();
         $this->traits     = array();
         $this->functions  = array();
-        $class            = false;
-        $classEndLine     = false;
+        $class            = array();
+        $classEndLine     = array();
         $trait            = false;
         $traitEndLine     = false;
         $interface        = false;
@@ -395,11 +395,11 @@ class PHP_Token_Stream implements ArrayAccess, Countable, SeekableIterator
                     );
 
                     if ($token instanceof PHP_Token_CLASS) {
-                        $class        = $token->getName();
-                        $classEndLine = $token->getEndLine();
+                        $class[]        = $token->getName();
+                        $classEndLine[] = $token->getEndLine();
 
-                        if ($class != 'anonymous class') {
-                            $this->classes[$class] = $tmp;
+                        if ($class[count($class)-1] != 'anonymous class') {
+                            $this->classes[$class[count($class)-1]] = $tmp;
                         }
                     } else {
                         $trait                = $token->getName();
@@ -421,7 +421,7 @@ class PHP_Token_Stream implements ArrayAccess, Countable, SeekableIterator
                       'file'      => $this->filename
                     );
 
-                    if ($class === false &&
+                    if (empty($class) &&
                         $trait === false &&
                         $interface === false) {
                         $this->functions[$name] = $tmp;
@@ -431,8 +431,8 @@ class PHP_Token_Stream implements ArrayAccess, Countable, SeekableIterator
                             $tmp['startLine'],
                             $tmp['endLine']
                         );
-                    } elseif ($class !== false && $class != 'anonymous class') {
-                        $this->classes[$class]['methods'][$name] = $tmp;
+                    } elseif (!empty($class) && $class[count($class)-1] != 'anonymous class') {
+                        $this->classes[$class[count($class)-1]]['methods'][$name] = $tmp;
 
                         $this->addFunctionToMap(
                             $class . '::' . $name,
@@ -453,10 +453,10 @@ class PHP_Token_Stream implements ArrayAccess, Countable, SeekableIterator
                     break;
 
                 case 'PHP_Token_CLOSE_CURLY':
-                    if ($classEndLine !== false &&
-                        $classEndLine == $token->getLine()) {
-                        $class        = false;
-                        $classEndLine = false;
+                    if (!empty($classEndLine) &&
+                        $classEndLine[count($classEndLine)-1] == $token->getLine()) {
+                        array_pop($classEndLine);
+                        array_pop($class);
                     } elseif ($traitEndLine !== false &&
                         $traitEndLine == $token->getLine()) {
                         $trait        = false;

commit c897717eb25098ec199d2ebd77095aeeb4c17295

Fix array to string conversion
Regression introduced in 6f1447a60f068c89d345ceee32025a51dd630f1c

src/Token/Stream.php
@@ -435,7 +435,7 @@ class PHP_Token_Stream implements ArrayAccess, Countable, SeekableIterator
                         $this->classes[$class[count($class)-1]]['methods'][$name] = $tmp;
 
                         $this->addFunctionToMap(
-                            $class . '::' . $name,
+                            $class[count($class)-1] . '::' . $name,
                             $tmp['startLine'],
                             $tmp['endLine']
                         );