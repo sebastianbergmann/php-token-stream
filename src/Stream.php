7513ea01fab8388ac655c2badec77a0c3ce289cf
Sebastian Bergmann <sb@sebastian-bergmann.de>
2020-06-27 11:04:45 +0200

    Fix type error

--- src/Stream.php
+++ src/Stream.php
@@ -448,11 +448,11 @@ class PHP_Token_Stream implements ArrayAccess, Countable, SeekableIterator
              * @see https://github.com/sebastianbergmann/php-token-stream/issues/95
              */
             if (PHP_MAJOR_VERSION >= 8 &&
-                $name === 'WHITESPACE' &&                               // Current token is T_WHITESPACE
-                isset($this->tokens[$id - 1]) &&                        // Current token is not the first token
-                $this->tokens[$id - 1] instanceof PHP_Token_COMMENT &&  // Previous token is T_COMMENT
-                strpos($this->tokens[$id - 1], '/*') === false &&       // Previous token is comment that starts with '#' or '//'
-                strpos($text, "\n") === 0                               // Text of current token begins with newline
+                $name === 'WHITESPACE' &&                                  // Current token is T_WHITESPACE
+                isset($this->tokens[$id - 1]) &&                           // Current token is not the first token
+                $this->tokens[$id - 1] instanceof PHP_Token_COMMENT &&     // Previous token is T_COMMENT
+                strpos((string) $this->tokens[$id - 1], '/*') === false && // Previous token is comment that starts with '#' or '//'
+                strpos($text, "\n") === 0                                  // Text of current token begins with newline
             ) {
                 $this->tokens[$id - 1] = new PHP_Token_COMMENT(
                     $this->tokens[$id - 1] . "\n",
